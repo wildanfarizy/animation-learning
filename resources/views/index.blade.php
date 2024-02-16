c5d6565f82f993517cee8d692bf83b532f51dafe
firhan <mfirhan20@gmail.com>
2024-02-16 15:04:29 +0700

    s

--- resources/views/index.blade.php
+++ resources/views/index.blade.php
@@ -1,10 +1,10 @@
-@extends('layout.main')
+@extends('layouts.main')
 @section('body')
 <div class="container">
     <div class="content">
         <div class="card">
             <div class="card-body">
-                
+                <p>a</p>
             </div>
         </div>
     </div>
