1579bce65da634e3ab894110ca3803c6d325b61c
firhan <mfirhan20@gmail.com>
2024-02-16 14:59:57 +0700

    selesai

--- resources/views/index.blade.php
+++ resources/views/index.blade.php
@@ -1,12 +1,12 @@
-<!DOCTYPE html>
-<html lang="en">
-<head>
-    <meta charset="UTF-8">
-    <meta name="viewport" content="width=device-width, initial-scale=1.0">
-    <meta http-equiv="X-UA-Compatible" content="ie=edge">
-    <title>Document</title>
-</head>
-<body>
-
-</body>
-</html>
+@extends('layout.main')
+@section('body')
+<div class="container">
+    <div class="content">
+        <div class="card">
+            <div class="card-body">
+                
+            </div>
+        </div>
+    </div>
+</div>
+@endsection
